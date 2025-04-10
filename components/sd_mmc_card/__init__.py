97d124a2f22b7ab71dcab5f3ea205a1a102fd546
youkorr <96953496+youkorr@users.noreply.github.com>
2025-04-10 05:41:46 +0200

    Update __init__.py

--- components/sd_mmc_card/__init__.py
+++ components/sd_mmc_card/__init__.py
@@ -20,6 +20,7 @@ CONF_DATA1_PIN = "data1_pin"
 CONF_DATA2_PIN = "data2_pin"
 CONF_DATA3_PIN = "data3_pin"
 CONF_MODE_1BIT = "mode_1bit"
+CONF_HIGH_SPEED_MODE = "high_speed_mode"
 CONF_POWER_CTRL_PIN = "power_ctrl_pin"
 
 sd_mmc_card_component_ns = cg.esphome_ns.namespace("sd_mmc_card")
@@ -44,6 +45,7 @@ CONFIG_SCHEMA = cv.Schema(
         cv.Optional(CONF_DATA2_PIN): pins.internal_gpio_pin_number({CONF_OUTPUT: True, CONF_INPUT: True}),
         cv.Optional(CONF_DATA3_PIN): pins.internal_gpio_pin_number({CONF_OUTPUT: True, CONF_INPUT: True}),
         cv.Optional(CONF_MODE_1BIT, default=False): cv.boolean,
+        cv.Optional(CONF_HIGH_SPEED_MODE, default=False): cv.boolean,
         cv.Optional(CONF_POWER_CTRL_PIN) : pins.gpio_pin_schema({
             CONF_OUTPUT: True,
             CONF_PULLUP: False,
@@ -55,18 +57,15 @@ CONFIG_SCHEMA = cv.Schema(
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
-
     cg.add(var.set_mode_1bit(config[CONF_MODE_1BIT]))
-
+    cg.add(var.set_high_speed_mode(config[CONF_HIGH_SPEED_MODE]))
     cg.add(var.set_clk_pin(config[CONF_CLK_PIN]))
     cg.add(var.set_cmd_pin(config[CONF_CMD_PIN]))
     cg.add(var.set_data0_pin(config[CONF_DATA0_PIN]))
-
     if (config[CONF_MODE_1BIT] == False):
         cg.add(var.set_data1_pin(config[CONF_DATA1_PIN]))
         cg.add(var.set_data2_pin(config[CONF_DATA2_PIN]))
         cg.add(var.set_data3_pin(config[CONF_DATA3_PIN]))
-
     if (CONF_POWER_CTRL_PIN in config):
         power_ctrl = await cg.gpio_pin_expression(config[CONF_POWER_CTRL_PIN])
         cg.add(var.set_power_ctrl_pin(power_ctrl))
